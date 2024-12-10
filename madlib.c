2768d3586e9eefd7ae0887ebb19ef50ad990bdcb
Carter Brashear <carterbrashear@gmail.com>
2024-12-10 15:54:24 -0800

    Working project.

--- madlib.c
+++ madlib.c
@@ -1,10 +1,11 @@
+// Authors: Carter Brashear, Raidon Santos, 
+// CS135 final project: madlib
 #include <stdio.h>
 
 #define DEFAULT_INPUT_FILE "madlib1.txt"
 #define MAX_LINE_LEN 100
 #define MAX_NUM_LINES 100
 
-int open_input_file(FILE *input_file, int argc, char **argv);
 void load_input_file(FILE *input_file, char file_buff[MAX_NUM_LINES][MAX_LINE_LEN]);
 void print_buffer(char buffer[MAX_NUM_LINES][MAX_LINE_LEN]);
 void get_words(char buffer[MAX_NUM_LINES][MAX_LINE_LEN]);
@@ -13,20 +14,18 @@ void remove_newlines(char file_buff[MAX_NUM_LINES][MAX_LINE_LEN]);
 
 int main(int argc, char **argv) {
   FILE *input_file;
-  //  printf("DEBUG: OPENGING FILE...\n");
   if (argc <= 1) {
     // No arguments were passed into the executable so load the default file
-    printf("File not found, reverting to base file.");
     input_file = fopen(DEFAULT_INPUT_FILE, "r");
     if (input_file == NULL) {
-      // TODO: Print error message
+      printf("ERROR: Failed to open file.");
       return 0;
     }
   } else {
     // An argument was passed into the executable so use it as the filename
     input_file = fopen(argv[1], "r");
     if (input_file == NULL) {
-      // TODO: Print error message
+      printf("ERROR: Failed to open file %s", argv[1]);
       return 0;
     }
   }
@@ -42,26 +41,6 @@ int main(int argc, char **argv) {
   print_buffer(file_buff);
 }
 
-int open_input_file(FILE *input_file, int argc, char **argv) {
-  if (argc <= 1) {
-    // No arguments were passed into the executable so load the default file
-    input_file = fopen(DEFAULT_INPUT_FILE, "r");
-    if (input_file == NULL) {
-      // TODO: Print error message
-      printf("File not found, reverting to base file.");
-      return 0;
-    }
-  } else {
-    // An argument was passed into the executable so use it as the filename
-    input_file = fopen(argv[1], "r");
-    if (input_file == NULL) {
-      // TODO: Print error message
-      return 0;
-    }
-  }
-  return 1;
-}
-
 void load_input_file(FILE *input_file, char file_buff[MAX_NUM_LINES][MAX_LINE_LEN]) {
   for (int i = 0; i < MAX_NUM_LINES; i++) {
     fgets(file_buff[i], MAX_LINE_LEN, input_file);
@@ -75,6 +54,7 @@ void print_buffer(char buffer[MAX_NUM_LINES][MAX_LINE_LEN]) {
   for (int i = 0; buffer[i][0] != '\0'; i++) {
     printf("%s", buffer[i]);
   }
+  printf("\n");
 }
 
 void get_words(char buffer[MAX_NUM_LINES][MAX_LINE_LEN]) {
@@ -108,6 +88,25 @@ void get_word(char* destination, char word_type) {
       printf("Enter a Verb: ");
       scanf("%s", destination + 1);
       break;
+  }
+  // Add space afterwards
+  char next_char = *(destination + MAX_LINE_LEN);
+  switch (next_char) {
+    case '.':
+    case ':':
+    case '!':
+    case ',':
+    case ';':
+    case '?':
+    case '"':
+      break;
+    default: {
+      int i = 0;
+      while (destination[i] != '\0') {
+        i++;
+      }
+      destination[i] = ' ';
+    }
   }
 }
 

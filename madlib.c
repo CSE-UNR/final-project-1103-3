c21356307f6baee38d94d8ffdbbeaaad7c4aa8e1
Carter Brashear <cbrashear@unr.edu>
2024-12-10 20:00:07 +0000

    Breaking up some functions.

--- madlib.c
+++ madlib.c
@@ -1,3 +1,6 @@
+// Author: Pearce Robinson, Raidon Santos, Carter Brashear
+// 12/9/2024
+// Madlib final project
 #include <stdio.h>
 
 #define FILE_BUFF_MAX 5000
@@ -5,6 +8,8 @@
 #define MAX_INPUT_LEN 50
 
 void get_word(char* input, char type);
+void print_result(char* filebuff, char inputs[MAX_NUM_INPUTS][MAX_INPUT_LEN]);
+void punctuation_fix(char* filebuff, char inputs[MAX_NUM_INPUTS][MAX_INPUT_LEN], int char_index, int input_index);
 
 int main(int argc, char** argv) {
 	if (argc <= 1) {
@@ -60,36 +65,9 @@ int main(int argc, char** argv) {
 
 	file_buff[i - 1] = 0;
 
-	// Print the output.
-	input_index = 0;
-	i = 0;
-	do {
-		if (file_buff[i] == '\n') {
-			switch(file_buff[i+1]) {
-				case '.':
-					printf(" %s", inputs[input_index]);
-					break;
-				case ',':
-					printf(" %s", inputs[input_index]);
-					break;
-				case '!':
-					printf(" %s", inputs[input_index]);
-					break;
-				case '?':
-					printf(" %s", inputs[input_index]);
-					break;
-				default:
-					printf(" %s ", inputs[input_index]);
-					break;
-			}
-			input_index++;
-		}
-		else {
-			printf("%c", file_buff[i]);
-		}
-		i++;
-	} while (file_buff[i] != '\0'); 
-	printf("\n");
+	
+	print_result(file_buff, inputs);
+
 	return 0;
 }
 
@@ -106,5 +84,42 @@ void get_word(char* input, char type) {
 			break;
 	}
 	scanf("%s", input);
+}
+
+void print_result(char* filebuff, char inputs[MAX_NUM_INPUTS][MAX_INPUT_LEN]){
+	// Print the output.
+	int input_index = 0;
+	int i = 0;
+	do {
+		if (filebuff[i] == '\n') {
+			punctuation_fix(filebuff, inputs, i, input_index);
+			input_index++;
+		}
+		else {
+			printf("%c", filebuff[i]);
+		}
+		i++;
+	} while (filebuff[i] != '\0'); 
+	printf("\n");
+}
+
+void punctuation_fix(char* filebuff, char inputs[MAX_NUM_INPUTS][MAX_INPUT_LEN], int char_index, int input_index) {
+	switch(filebuff[char_index+1]) {
+		case '.':
+			printf(" %s", inputs[input_index]);
+			break;
+		case ',':
+			printf(" %s", inputs[input_index]);
+			break;
+		case '!':
+			printf(" %s", inputs[input_index]);
+			break;
+		case '?':
+			printf(" %s", inputs[input_index]);
+			break;
+		default:
+			printf(" %s ", inputs[input_index]);
+			break;
+	}
 }
 
